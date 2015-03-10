bd90b169a5f33c2749bf420cb16629d89e49d7c5
davidroffe <davidroffe@gmail.com>
2015-03-09 22:58:13 -0700

    Delete node.js

--- app/models/node.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var noteSchema = mongoose.Schema({
-	car: mongoose.Schema.Types.ObjectId,
-	name  : String,
-	message  : String
-});
-
-module.exports = mongoose.model('Note', carSchema);
